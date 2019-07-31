e50b45282f80d4b57f775aa7f3e12f9cbeb852fb
Ilya <h82st8@gmail.com>
2019-07-31 16:16:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.modal').modal('show');
+    // setTimeout(function(){$('.modal').fadeOut();}, 5000);
+
+  //   $('#myClose').on('click', function() {
+  //   setTimeout(function(){$('#myModal').fadeOut();}, 5000);
+  // });
+
+  //   $('#myClose').click(function() {
+  //   setTimeout(function(){$(this).parents('.modal').fadeOut();}, 5000);
+  // });
+
+});
